2b19d5d0a2efc8bb61dd826cc2fd739e821b2ecb
S2200155\Snetsystems <bnt10@naver.com>
2022-08-26 18:40:46 +0900

    Test add code function to trasnform camel case colors to spaced colors

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import {render, screen, fireEvent} from '@testing-library/react'
 import App from './App'
-
+import {replaceCamelWithSpaces} from './App'
 // test('renders learn react link', () => {
 //   render(<App />)
 //   const linkElement = screen.getByRole('link', {name: /learn react/i})
@@ -53,4 +53,17 @@ test('Checkbox disables button on first click and enables on second click', () =
 
   fireEvent.click(checkbox)
   expect(colorButton).toBeEnabled()
+})
+
+describe('spaces before camel-case capital letters', () => {
+  test('Works for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red')
+  })
+  test('Works for one inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue')
+  })
+
+  test('Works for multiple inner capital letters', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red')
+  })
 })
