dfc2c5e279580a6711a139602fe5c17430556b4c
S2200155\Snetsystems <bnt10@naver.com>
2022-08-27 16:17:55 +0900

    test and code async unctionality

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,11 +1,24 @@
 import {useState} from 'react'
 import {Button} from 'react-bootstrap'
 import Form from 'react-bootstrap/Form'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Popover from 'react-bootstrap/Popover'
+
 function SummaryForm() {
   const [checkboxChecked, setCheckboxChecked] = useState(false)
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  )
+
   const checkboxLabel = (
     <span>
-      I agree to <span style={{color: 'blue'}}>Terms and Conditions</span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{color: 'blue'}}> Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   )
   return (
